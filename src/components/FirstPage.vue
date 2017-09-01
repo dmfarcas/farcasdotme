c89038d399220b08260ac69f6c1d464c31402d1e
dmfarcas <dan.farcas@assist.ro>
2017-09-01 17:54:31 +0300

    Polishing

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="hello">
+    
     <p>This is another terminal portfolio. Type 'pls stuff' for stuff.</p>
     <pre>{{ linesToBeDisplayed }}</pre> 
     <span>$ </span><input autofocus v-on:keyup.enter="bamboozle" v-model="terminalText"></input>
-
   </div>
 </template>
 
@@ -16,7 +16,8 @@ export default {
     return {
       terminalText: '',
       linesToBeDisplayed: '',
-      counter: 0
+      counter: 0,
+      successCounter: null
     }
   },
   methods: {
@@ -25,18 +26,26 @@ export default {
 
        if (this.counter === 3) {
          this.counter += 1;
-         this.linesToBeDisplayed += "If you insist...\n"
-         this.terminalText = "";
-         return;
-       }
-       if(this.terminalText === 'pls stuff') {
-         this.linesToBeDisplayed += "> Awesome! Going to the next view. Heeere it comes!\n"
+         this.linesToBeDisplayed += "> If you insist..."
+       } else if(this.terminalText === 'pls stuff') {
+         this.linesToBeDisplayed += "> Awesome! Going to the next view. Heeere it comes!"
+         this.successCounter = 3;
+         const successInterval = setInterval(() => {
+         if (this.successCounter === 0) {
+            clearInterval(successInterval);
+            this.linesToBeDisplayed += "> Done!\n"
+            return;
+          }
+           this.linesToBeDisplayed += `> ${this.successCounter} \n`
+           this.successCounter -= 1;
+         }, 1000);
        } else {
          this.counter +=1;
-         this.linesToBeDisplayed += "> You don't like to do what you're told, eh?\n"
+         this.linesToBeDisplayed += "> You don't like to do what you're told, eh?"
        }
-       this.terminalText = "";
 
+       this.linesToBeDisplayed += "\n";
+       this.terminalText = "";
     }
   }
 }
@@ -49,6 +58,21 @@ export default {
   background-color: #000000; 
   color: #99ff00;
 }
+.x:after {
+      content: "Loading ...";
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      font-size: 24px;
+      color: #fff;
+      background-color: rgba(mediumspringgreen, 0.5);
+}
+
 input {
   outline: none;
   border: 0 solid red;
@@ -59,7 +83,6 @@ input:focus {
     border: none;
 }
 p, input, span, pre {
-  @import url('https://fonts.googleapis.com/css?family=VT323');
   color: #99ff00;
   font-size:2em;
   letter-spacing: 0.1em;
