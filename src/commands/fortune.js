f9a27e90c89ce52d28b7d0498fb3b5d959611755
Dan Farcas <dan@farcas.me>
2017-09-16 08:22:41 +0300

    Spring cleaning

--- src/commands/fortune.js
+++
@@ -1,9 +0,0 @@
-export default async function fortune(cb) {
-     await fetch('http://www.yerkee.com/api/fortune')
-    .then((response) => {
-        return response.json();
-      })
-      .then(async (json) => {
-        await json.fortune;
-    });
-}
--- src/commands/help.js
+++
@@ -1,3 +0,0 @@
-export default function help() {
-    return `Hello, help!`
-}
