6bfce5b10e697bf3eb7da5547317c033b085bfb7
dmfarcas <dan.farcas@3pillarglobal.com>
2018-10-09 17:01:18 +0300

    Formatted code

--- src/lib/help.js
+++ src/lib/help.js
@@ -1,3 +1,3 @@
 export default function help() {
-    return `give <thing> -- returns a picture of a <thing>. Try 'give cats'.`
+  return `give <thing> -- returns a picture of a <thing>. Try 'give cats'.`;
 }
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,9 +1,9 @@
-export const bringBackFocus = (el) => {
+export const bringBackFocus = el => {
   window.onkeydown = () => document.querySelector(el).focus(); // returning focus on keydowwn
-}
+};
 
-export const scrollToBottom = (el) => {
-   const content = document.querySelector(el);
-   const height = content.scrollHeight
-   window.scrollTo(0, height);
- }
+export const scrollToBottom = el => {
+  const content = document.querySelector(el);
+  const height = content.scrollHeight;
+  window.scrollTo(0, height);
+};
